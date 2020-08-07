f73783c6cceef7cb1aa0042fb3525670f2f21cec
Mark Sellors <github@5vcc.com>
2020-08-08 00:43:31 +0100

    updated to support paged results and offsets

--- R/get_group.R
+++ R/get_group.R
@@ -1,17 +1,26 @@
 #' Returns the member usernames from a given group
 #'
 #' @param groupname Valid discourse group name
+#' @param paged Boolean that controls whether we fetch additional pages is required
+#' @param offset int number of records to offset the query - only applicable if paged = TRUE
 #'
 #' @export
-get_group_members <- function(groupname){
+get_group_members <- function(groupname, paged = FALSE, offset = 0){
 
   DISCOURSE_SERVER <- read_server_env_var()
+
+  if (paged == TRUE){
+    offset_query <- paste0("?offset=", offset)
+  } else {
+    offset_query <- ""
+  }
 
   URL <- paste0(
     DISCOURSE_SERVER,
     "/groups/",
     groupname,
-    "/members.json"
+    "/members.json",
+    offset_query
     )
 
   memoised_get <- memoise::memoise(
@@ -20,11 +29,17 @@ get_group_members <- function(groupname){
 
   response <- memoised_get(URL)
 
+  response_content <- httr::content(response)
+
   if (httr::http_status(response)$category != "Success"){
-    stop("Request was not successful. Please check the name and try aagin")
+    stop("Request was not successful. ", response_content$errors[[1]])
   }
 
-  response_content <- httr::content(response)
+  if ( response_content$meta$total > response_content$meta$limit && paged == FALSE){
+    URL <- paste0(URL, "?limit=", response_content$meta$total)
+    response <- memoised_get(URL)
+    response_content <- httr::content(response)
+  }
 
   num_members <- length(response_content$members)
 
